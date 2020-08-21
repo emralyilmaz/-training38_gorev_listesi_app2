481054ffb41ed821d1b884136783086a34577cf5
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-21 23:10:42 +0300

    Consumer Yöntemi- provider.of'lar kısaltıldı

--- lib/pages/gorevler_list.dart
+++ lib/pages/gorevler_list.dart
@@ -6,17 +6,19 @@ import 'package:provider/provider.dart';
 class GorevListesi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: Provider.of<GorevVeri>(context).gorevListesi.length,
-      itemBuilder: (context, index) => GorevTile(
-        gorevAd: Provider.of<GorevVeri>(context).gorevListesi[index].gorevAd,
-        secim: Provider.of<GorevVeri>(context).gorevListesi[index].yapildi,
-        checkboxCallBack: (bool val) {
-          // setState(() {
-          //   Provider.of<GorevVeri>(context).gorevListesi[index].toggleYapildi();
-          // });
-        },
-      ),
-    );
+    return Consumer<GorevVeri>(builder: (context, gorevVeri, child) {
+      return ListView.builder(
+        itemCount: Provider.of<GorevVeri>(context).gorevSayisi,
+        itemBuilder: (context, index) => GorevTile(
+          gorevAd: Provider.of<GorevVeri>(context).gorevListesi[index].gorevAd,
+          secim: Provider.of<GorevVeri>(context).gorevListesi[index].yapildi,
+          checkboxCallBack: (bool val) {
+            // setState(() {
+            //   Provider.of<GorevVeri>(context).gorevListesi[index].toggleYapildi();
+            // });
+          },
+        ),
+      );
+    });
   }
 }
