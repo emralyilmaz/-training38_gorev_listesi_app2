8952d9c288b4efeb82991fee49b10a5b7bff9a6c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-21 16:44:32 +0300

    Global State İle Checkbox Kontrol

--- lib/pages/gorevler_tile.dart
+++ lib/pages/gorevler_tile.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 
-class GorevTile extends StatelessWidget {
+class GorevTile extends StatefulWidget {
+  @override
+  _GorevTileState createState() => _GorevTileState();
+}
+
+class _GorevTileState extends State<GorevTile> {
+  bool secim = false;
+  void secimDegistirmeFonk(bool yeniSecim) {
+    setState(() {
+      secim = yeniSecim;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         "gorev1",
         style: TextStyle(
+          decoration: secim ? TextDecoration.lineThrough : null,
           color: Color.fromRGBO(228, 245, 177, 1),
           fontSize: 20,
         ),
       ),
-      trailing: Checkbox(value: false, onChanged: (bool val) {}),
+      trailing: GorevCheckBox(
+        secim: secim,
+        secimDegistir: secimDegistirmeFonk,
+      ),
+    );
+  }
+}
+
+class GorevCheckBox extends StatelessWidget {
+  final bool secim;
+  final Function secimDegistir;
+
+  GorevCheckBox({this.secim, this.secimDegistir});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: secim,
+      onChanged: secimDegistir,
+      activeColor: Color.fromRGBO(81, 43, 82, 1),
     );
   }
 }
